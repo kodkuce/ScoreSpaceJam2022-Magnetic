5f604bf5e58ac6106eb204c1b4534e9ffda1615a
kodkuce <kodkuce@mail.ru>
2022-04-30 22:00:10 +0200

    leaderboard close btn

--- Magnetic/Leaderboard/Leaderboard.cs
+++ Magnetic/Leaderboard/Leaderboard.cs
@@ -5,18 +5,38 @@ public class Leaderboard : Control
 {
     [Export] public NodePath leaderboardEntriesRoot;
     [Export] public NodePath leaderboardRoot;
+    [Export] public NodePath closeButton;
 
 
     public override void _EnterTree()
     {
         base._EnterTree();
+        GameEvents.ShowLeaderboard += OnShowLeaderboard;
     }
+
 
     public override void _ExitTree()
     {
         base._ExitTree();
+        GameEvents.ShowLeaderboard -= OnShowLeaderboard;
     }
 
+    public override void _Ready()
+    {
+        base._Ready();
+        GetNode<Button>(closeButton).Connect("button_up", this, nameof(CloseLeaderboard));
+    }
+
+    private void OnShowLeaderboard()
+    {
+        FillWithData();
+        GetNode<Control>(leaderboardRoot).Visible = true;
+    }
+
+    private void CloseLeaderboard()
+    {
+        GetNode<Control>(leaderboardRoot).Visible = false;
+    }
 
     public void FillWithData()
     {
